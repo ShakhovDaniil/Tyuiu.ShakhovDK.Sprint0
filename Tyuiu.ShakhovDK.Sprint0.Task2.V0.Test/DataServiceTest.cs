cd62b91fb1dd6bc4d1e0b8108fd7f050536b442a
ShakhovDaniil <shakhov.daniil.konstantinovich@gmail.com>
2024-09-15 00:33:31 +0500

    Task2

---
+++ Tyuiu.ShakhovDK.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShakhovDK.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ShakhovDK.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Даниил";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Даниил", res);
+        }
+    }
+}
---
+++ Tyuiu.ShakhovDK.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShakhovDK.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShakhovDK.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a9e9f95-82df-4465-9895-5a740b8c2ef4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
